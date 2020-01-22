39a749636d032e6ea63de43e6cd97c9309d7c8e4
Noah Holt <noahryanholt@gmail.com>
2020-01-22 09:18:05 -0600

    Changed to have username

--- Users.sql
+++ Users.sql
@@ -1,23 +1,22 @@
 CREATE EXTENSION pgcrypto; --Allows PostgreSQL to understand UUIDS.  Only have to create the extension once for a database
 
+-- DROP TABLE users;
+
 CREATE TABLE users (
         id uuid NOT NULL DEFAULT gen_random_uuid(),
-        firstname VARCHAR(255) NOT NULL DEFAULT(''),
-        lastname VARCHAR(255) NOT NULL DEFAULT(''),
+        username VARCHAR(255) NOT NULL,
         userid SERIAL,
         password CHAR(64),
         createdon TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
         CONSTRAINT user_pkey PRIMARY KEY (id) 
 );
 
-INSERT INTO users (firstname, lastname, password) VALUES (
-        'John',
-        'Smith',
+INSERT INTO users (username, password) VALUES (
+        'johnsmith',
         'password123'
 );
 
-INSERT INTO users (firstname, lastname, password) VALUES (
-        'Jane',
-        'Doe',
+INSERT INTO users (username, password) VALUES (
+        'janedoe',
         'password321'
 );
