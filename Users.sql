f521004cdeac490270a1d1eee531f1fb5d902caa
Tyler Gay <starlank@gmail.com>
2020-04-24 18:48:43 -0500

    Added new columns

--- Users.sql
+++ Users.sql
@@ -2,13 +2,16 @@ CREATE EXTENSION pgcrypto; --Allows PostgreSQL to understand UUIDS.  Only have t
 
 DROP TABLE users;
 
-CREATE TABLE users (
-        userid SERIAL,
-        username VARCHAR(255) NOT NULL,
-        password TEXT,
-        misc_data JSON,
-        createdon TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT now(),
-        CONSTRAINT user_pkey PRIMARY KEY (userid) 
+CREATE TABLE "users" (
+	"userid" INTEGER NOT NULL DEFAULT 'nextval(''users_userid_seq''::regclass)',
+	"username" VARCHAR(255) NOT NULL,
+	"password" TEXT NULL DEFAULT NULL,
+	"createdon" TIMESTAMP NOT NULL DEFAULT 'now()',
+	"spotify_playlists" JSON NULL DEFAULT NULL,
+	"spotify_accesstoken" VARCHAR(255) NULL DEFAULT 'NULL::character varying',
+	"spotify_refreshtoken" VARCHAR(255) NULL DEFAULT 'NULL::character varying',
+	"spotify_id" VARCHAR(255) NULL DEFAULT 'NULL::character varying',
+	PRIMARY KEY ("userid")
 );
 
 INSERT INTO users (username, password) VALUES (
